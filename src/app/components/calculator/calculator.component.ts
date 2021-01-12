8e61e4783eadff1ebe80258727767c156c6931e0
LeonardoFM <leoofissicaro@hotmail.com>
2021-01-12 19:15:13 -0300

    primeira tela de calculo resgate de valores input

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -8,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   visible: boolean = false;
+  form1: boolean = true;
+  form2: boolean = false;
+  form3: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  forms(){
+    if(this.form1 == true && this.form2 == this.form3){
+      this.form2 = true;
+      this.form1 = !this.form2;
+    } else {
+      if(this.form2 == true && this.form1 == this.form3){
+        this.form2 = false;
+        this.form3 = !this.form2;
+      }
+    }
   }
 
 }
